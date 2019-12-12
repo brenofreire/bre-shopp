3b93cb41cc5f6a5a837b47cbfc38fcbc025780d5
Breno Freire <breno.freire@cubos.io>
2019-12-12 17:47:05 -0300

    Implementa styled components no botão de voltar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { observer } from "mobx-react-lite" 
+import { observer } from "mobx-react-lite"
 import { useStores } from '../../stores/RootStore';
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
 const Header: React.FC = props => {
-    const { generalStore } = useStores();    
-    
+    const { generalStore } = useStores();
+
     const { productsOnCart, cartValue } = generalStore;
 
+    const StyledLink = styled(Link)`
+        background: #fff;
+        border-radius: 10px;
+        border: 0px;
+        box-shadow: 0px 3px 3px #eee;
+        color: #333;
+        align-self: center;
+        padding: 5px;
+        text-decoration: none;
+    `;
+
     const rendersBackBuck = () => {
-        if(window.location.pathname != '/') {
-            return <Link to='/'>Voltar</Link>
+        if (window.location.pathname != '/') {
+            return <StyledLink to='/'>← Voltar</StyledLink>
         } else {
             return null;
         }
