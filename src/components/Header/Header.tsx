4554d02cdd1e63e5a91b5c64350da6708e90a9f8
Breno Freire <breno.freire@cubos.io>
2019-12-12 17:06:47 -0300

    implementa botão de voltar para verificar controle de estado global

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
 import { observer } from "mobx-react-lite" 
 import { useStores } from '../../stores/RootStore';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 
-const Header: React.FC = () => {
+const Header: React.FC = props => {
     const { generalStore } = useStores();    
     
     const { productsOnCart, cartValue } = generalStore;
 
+    const rendersBackBuck = () => {
+        if(window.location.pathname != '/') {
+            return <Link to='/'>Voltar</Link>
+        } else {
+            return null;
+        }
+    }
+
     return (
         <header style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {rendersBackBuck()}
             <h1>Bre-shopp</h1>
             <span style={{ alignSelf: 'center' }}>
                 {productsOnCart} itens {' '}
