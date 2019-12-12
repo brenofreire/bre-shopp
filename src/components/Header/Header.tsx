84824567db0292b621f365da1de4fd90ee29283a
Breno Freire <breno.freire@cubos.io>
2019-12-12 17:30:26 -0300

    Implementa uso do hooks de estado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { observer } from "mobx-react-lite" 
 import { useStores } from '../../stores/RootStore';
-import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = props => {
--- src/components/Product/ProductSingle.tsx
+++ src/components/Product/ProductSingle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStores } from '../../stores/RootStore';
 
 interface IProps {
@@ -8,15 +8,23 @@ interface IProps {
 const ProductSingle: React.FC<IProps> = (props) => {
     const { generalStore } = useStores();
     const { product } = props;
+
+    const [cor, setCor ] = useState(0);
     
     const addToCart = (price: number) => {
         generalStore.setCartInfo(price);  
-        console.log('>>>', generalStore.productsOnCart, generalStore.cartValue);
     }
+
+    const changesColor = (direction: string) => {
+        direction === 'next' ? setCor(cor+1) : setCor(cor-1);
+    } 
+
     return (
         <div style={{ textAlignLast: 'center' }}>
             <h1>{product.name}</h1>
-            <img src="https://picsum.photos/250" alt="Lorem pic" />
+            <img src="https://picsum.photos/250" alt="Lorem pic" style={{ border: `5px solid ${product.options.color[cor]}`}}/><br />
+            <button onClick={() => changesColor('prev')}>{'<'}</button>
+            <button onClick={() => changesColor('next')}>{'>'}</button>
             <p>{product.description}</p>
             <hr />
             <button onClick={() => addToCart(product.price)}>{product.price} Adicionar ao carrinho</button>
