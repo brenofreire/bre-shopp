364105ffc258b2c3fdce2498d10be9f5e5912289
Breno Freire <breno.freire@cubos.io>
2019-12-13 11:32:15 -0300

    implementa função de slide de estilos com setas

--- src/components/Product/ProductSingle.tsx
+++ src/components/Product/ProductSingle.tsx
@@ -9,20 +9,30 @@ const ProductSingle: React.FC<IProps> = (props) => {
     const { generalStore } = useStores();
     const { product } = props;
 
-    const [cor, setCor ] = useState(0);
-    
+    const [indexCor, setIndexCor] = useState(0);
+
     const addToCart = (price: number) => {
-        generalStore.setCartInfo(price);  
+        generalStore.setCartInfo(price);
     }
 
     const changesColor = (direction: string) => {
-        direction === 'next' ? setCor(cor+1) : setCor(cor-1);
-    } 
+        if (direction === 'next') {
+            if (product.options.color[indexCor + 1]) {
+                return setIndexCor(indexCor + 1);
+            } 
+            return setIndexCor(0);
+        } else {
+            if (product.options.color[indexCor - 1]) {
+                return setIndexCor(indexCor - 1);
+            }
+            return setIndexCor(product.options.color.length -1);
+        }
+    }
 
     return (
         <div style={{ textAlignLast: 'center' }}>
             <h1>{product.name}</h1>
-            <img src="https://picsum.photos/250" alt="Lorem pic" style={{ border: `5px solid ${product.options.color[cor]}`}}/><br />
+            <img src="https://picsum.photos/250" alt="Lorem pic" style={{ border: `5px solid ${product.options.color[indexCor]}` }} /><br />
             <button onClick={() => changesColor('prev')}>{'<'}</button>
             <button onClick={() => changesColor('next')}>{'>'}</button>
             <p>{product.description}</p>
